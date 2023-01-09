ca73c94a2e7982b823d420531629cc198116ca5d
Sourav2707 <smartsourav33@gmail.com>
2023-01-09 14:11:42 +0530

    UC2 Ability to create Linked List by adding 30 and 56 to 70

--- src/com/bridgelabs/day14_linkedlist/Day14LinkedList.java
+++ src/com/bridgelabs/day14_linkedlist/Day14LinkedList.java
@@ -22,6 +22,18 @@ public class Day14LinkedList {
         }
         System.out.println("Added "+node.data+" to the list");
     }
+    public void addFirst(int data) // add front to the list
+    {
+        Node node = new Node(data);
+        if (this.head == null) //head has the value 70 hence else part will execute
+            this.head = null;
+        else
+        {
+            node.next = this.head; //next reference is fed with 70 so the 30 will sit front of 70
+            this.head = node;
+        }
+        System.out.println("Added "+node.data+" to the front");
+    }
     public void get()
     {
         Node node = head; //pushing head data to temp. if head data is null the temp will be null and it will show as empty
